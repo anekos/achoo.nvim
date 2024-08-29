5d237f1991467154d19926b8099f67d86f0f3d19
anekos <anekos@snca.net>
2024-08-29 16:09:50 +0900

    Percent to Plus

--- lua/achoo/lib/plus.lua
+++ lua/achoo/lib/plus.lua
@@ -2,13 +2,13 @@ local M = {}
 
 function M.encode(s)
   local result = s:gsub('[^%w%-_~]', function(char)
-    return string.format('%%%02X', string.byte(char))
+    return string.format('+%02X', string.byte(char))
   end)
   return result
 end
 
 function M.decode(str)
-  local result = str:gsub('%%(%x%x)', function(hex)
+  local result = str:gsub('+(%x%x)', function(hex)
     return string.char(tonumber(hex, 16))
   end)
   return result
