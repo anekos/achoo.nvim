8a735578973bdef380bd0f60beebe4e915410bfa
anekos <anekos@snca.net>
2024-10-21 20:37:58 +0900

    Add Vim.modified function to check buffer modification status in session rotation logic.

--- lua/achoo/lib/vim.lua
+++ lua/achoo/lib/vim.lua
@@ -5,4 +5,22 @@ function M.command_line_escape(s)
   return result
 end
 
+function M.modified()
+  if 1 < vim.fn.bufnr() then
+    return true
+  end
+
+  if vim.bo.modified then
+    return true
+  end
+
+  for _, b in ipairs(vim.api.nvim_list_bufs()) do
+    if vim.api.nvim_get_option_value('modified', { buffer = b }) then
+      return true
+    end
+  end
+
+  return false
+end
+
 return M
